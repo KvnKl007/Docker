bc69afb502b3e5f8ec29ff1cf70a8ca96bf4bfc4
KvnKl007 <klakkent99@gmail.com>
2023-12-21 23:26:37 +0200

    my first docker

---
+++ first-container/README.md
@@ -0,0 +1,1 @@
+This is my first contianer and it's just basics nothing much going on just a start
---
+++ first-container/src/index.js
@@ -0,0 +1,21 @@
+// Import necessary modules
+const express = require('express');
+const path = require('path');
+
+// Create an Express application
+const app = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello, Docker World!');
+});
+
+// Start the server on port 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
